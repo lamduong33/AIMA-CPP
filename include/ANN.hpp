f25a0c0aca5f6100459535d9b769bfaef8efa976
Lam T Duong <lamthachduong@gmail.com>
2021-12-02 14:16:07 -0500

    Remaking neural net into an adjacency matrix.

--- include/ANN.hpp
+++ include/ANN.hpp
@@ -4,65 +4,16 @@
 #include <vector>
 #include <memory> // smart pointers
 #include <random> // RNG
-#include <cmath>
+#include <cmath> // math functions
 #include <string>
 
 using rng_engine = std::default_random_engine;
 using rng_distribution = std::uniform_real_distribution<float>;
 
-/* A neural node represents a list of connections of inputs being fed to it and
- * outputs that it will send forward.*/
-struct NeuralNode
+class NeuralNetwork
 {
-    // Inputs are a pair of <output, weight>
-    std::vector<std::pair<double, std::unique_ptr<NeuralNode>>> inputs;
-    // Outputs are a pair of <output, weight>
-    std::vector<std::pair<double, std::unique_ptr<NeuralNode>>> outputs;
+    // Represent the neural network as a weighted adjacency list
+    std::vector<std::pair<double, double>> adjacencyList;
 };
-
-/* A network input node will not have inputs. */
-class NetworkInput : NeuralNode
-{
-protected:
-    double inputValue; // input value is also the output value
-public:
-    // An input node MUST not have inputs, only outputs.
-    NetworkInput(double t_inputValue);
-    bool noInputs();
-};
-
-class NetworkOutput : NeuralNode
-{
-    std::string label;
-public:
-};
-
-/* A neuron is meant to be in the layers between the NetworkInput nodes and
- * NetworkOutput nodes.*/
-class Neuron : NeuralNode
-{
-    double bias;
-public:
-    Neuron();
-    double activationFunction();
-    double sigmoidFunction();
-    double reluFunction();
-    double getBias();
-    std::vector<std::pair<double, double>> getOutputs();
-    std::vector<std::pair<double, double>> getInputs();
-};
-
-template<class NeuralNode>
-struct Layer
-{
-    std::vector<NeuralNode> nodes;
-};
-
-class NeuralNetWork
-{
-    std::vector<NetworkInput> inputLayer;
-    std::vector<NetworkOutput> outputLayer;
-};
-
 
 #endif // ANN_H_
