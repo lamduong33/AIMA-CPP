807f52dd4154e4ac7dda1aff32f948bf3a4e745f
Lam T Duong <lamthachduong@gmail.com>
2021-12-02 22:36:19 -0500

    Reverting back to nodes instead of adjacency list.

--- include/ANN.hpp
+++ include/ANN.hpp
@@ -1,19 +1,51 @@
 #ifndef ANN_H_
 #define ANN_H_
 
-#include <vector>
+#include <cmath>
 #include <memory> // smart pointers
 #include <random> // RNG
-#include <cmath> // math functions
 #include <string>
+#include <vector>
 
 using rng_engine = std::default_random_engine;
-using rng_distribution = std::uniform_real_distribution<float>;
+using rng_distribution = std::uniform_real_distribution<double>;
 
-class NeuralNetwork
+/* A neuron is meant to be in the layers between the NetworkInput nodes and
+ * NetworkOutput nodes.*/
+class Neuron
 {
-    // Represent the neural network as a weighted adjacency list
-    std::vector<std::pair<double, double>> adjacencyList;
+    double bias;
+public:
+    Neuron();
+    double activationFunction();
+    double sigmoidFunction();
+    double reluFunction();
+    double getBias();
+    std::vector<std::pair<double, double>> getOutputs();
+    std::vector<std::pair<double, double>> getInputs();
+};
+
+class Weight
+{
+    Neuron source;
+    Neuron destination;
+    double weightValue;
+public:
+    Weight();
+    Weight(Neuron &t_source, Neuron &t_destination);
+    Weight(Neuron &t_source, Neuron &t_destination, double startRange,
+    double endRange);
+};
+
+template <class Neuron> struct Layer
+{
+    std::vector<Neuron> nodes;
+};
+
+class NeuralNetWork
+{
+    std::vector<Neuron> inputLayer;
+    std::vector<Neuron> outputLayer;
 };
 
 #endif // ANN_H_
--- src/ANN.cpp
+++ src/ANN.cpp
@@ -1,24 +1,37 @@
 #include "../include/ANN.hpp"
 
-NetworkInput::NetworkInput(double t_inputValue) : inputValue(t_inputValue) {}
+/* ============================== NEURON ===================================== */
+Neuron::Neuron() : bias{0.0} {}
+
+double Neuron::sigmoidFunction()
+{
+    return (1 / (1 + (exp(-activationFunction()))));
+}
+
+double Neuron::reluFunction() { return std::fmax(0.0, activationFunction()); }
 
 double Neuron::activationFunction()
 {
     double result = 0.0;
-    for (auto& input : this->inputs)
-    {
-        //result += input.first * input.second;
-    }
     result += this->bias;
     return result;
 }
 
-double Neuron::sigmoidFunction()
+/* ============================== WEIGHT ===================================== */
+
+Weight::Weight(Neuron &t_source, Neuron &t_destination)
+    : source{t_source}, destination{t_destination}
 {
-    return (1 / (1+(exp(-activationFunction()))));
+    rng_engine r{};
+    rng_distribution defaultRange{-5.5, 5.5};
+    weightValue = defaultRange(r);
 }
 
-double Neuron::reluFunction()
+Weight::Weight(Neuron &t_source, Neuron &t_destination,
+               double startRange, double endRange)
+    : source{t_source}, destination{t_destination}
 {
-    return std::fmax(0.0, activationFunction());
+    rng_engine r{};
+    rng_distribution range{startRange, endRange};
+    weightValue = range(r);
 }
