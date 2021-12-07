464b07fc8d858a0c3f780c25c521fc7167f16447
Lam T Duong <lamthachduong@gmail.com>
2021-12-07 15:00:41 -0500

    Weighted paths and graphs for neural net.

--- include/ANN.hpp
+++ include/ANN.hpp
@@ -14,38 +14,79 @@ using rng_distribution = std::uniform_real_distribution<double>;
  * NetworkOutput nodes.*/
 class Neuron
 {
+
     double bias;
+
 public:
+
     Neuron();
-    double activationFunction();
-    double sigmoidFunction();
-    double reluFunction();
+
+    /**
+     * Computes the series of inputs and weights given to a Neuron. This
+     * is actually not an activation function itself. It is meant to be used as an
+     * input for activation functions. */
+    double activationInput(std::vector<double>& inputs,
+                           std::vector<double>& weights);
+
+    /**
+     * Sigmoid function: 1 / (1 + e^-x) */
+    double sigmoidFunction(std::vector<double>& inputs,
+                           std::vector<double>& weights);
+
+    /**
+     * ReLU (rectifier) function: max(0,x) */
+    double reluFunction(std::vector<double>& inputs,
+                        std::vector<double>& weights);
+
     double getBias();
-    std::vector<std::pair<double, double>> getOutputs();
-    std::vector<std::pair<double, double>> getInputs();
+
 };
 
 class Weight
 {
-    Neuron source;
-    Neuron destination;
+    Neuron& source;
+    Neuron& destination;
     double weightValue;
+
 public:
     Weight();
-    Weight(Neuron &t_source, Neuron &t_destination);
-    Weight(Neuron &t_source, Neuron &t_destination, double startRange,
-    double endRange);
+    Weight(Neuron& t_source, Neuron& t_destination);
+    Weight(Neuron& t_source, Neuron& t_destination, double startRange,
+           double endRange);
+
+    Neuron getSource();
+    Neuron getDestination();
+
+    void setSource(Neuron& t_source);
+    void setDestination(Neuron& t_destination);
 };
 
-template <class Neuron> struct Layer
+/**
+ * Simple abstraction for a layer of neurons */
+struct Layer
 {
-    std::vector<Neuron> nodes;
+    std::vector<Neuron> layer;
 };
 
-class NeuralNetWork
+class NeuralNetwork
 {
-    std::vector<Neuron> inputLayer;
-    std::vector<Neuron> outputLayer;
+
+    Layer inputLayer;
+    std::vector<Layer> layers;
+    Layer outputLayer;
+
+public:
+
+    /**
+     * A constructor to create a barebone perceptron. */
+    NeuralNetwork();
+
+    /**
+     * Given input size */
+    NeuralNetwork(int t_inputSize);
+
+
+    // TODO: Constructor to construct neural network from text file.
 };
 
 #endif // ANN_H_
--- src/ANN.cpp
+++ src/ANN.cpp
@@ -1,37 +1,63 @@
 #include "../include/ANN.hpp"
 
 /* ============================== NEURON ===================================== */
+
 Neuron::Neuron() : bias{0.0} {}
 
-double Neuron::sigmoidFunction()
+double Neuron::sigmoidFunction(std::vector<double>& inputs,
+                               std::vector<double>& weights)
 {
-    return (1 / (1 + (exp(-activationFunction()))));
+    return (1 / (1 + (exp(-activationInput(inputs, weights)))));
 }
 
-double Neuron::reluFunction() { return std::fmax(0.0, activationFunction()); }
+/* ReLu function makes the net non-linear. */
+double Neuron::reluFunction(std::vector<double>& inputs,
+                            std::vector<double>& weights)
+{
+    return std::fmax(0.0, activationInput(inputs, weights));
+}
 
-double Neuron::activationFunction()
+double Neuron::activationInput(std::vector<double>& inputs,
+                               std::vector<double>& weights)
 {
     double result = 0.0;
-    result += this->bias;
-    return result;
+    if (inputs.size() != weights.size())
+        throw new std::exception;
+    for (int i = 0; i < (int)inputs.size(); i++)
+    {
+        result += inputs[i] * weights[i];
+    }
+    return result += this->bias;
 }
+
+double Neuron::getBias() { return this->bias; }
 
 /* ============================== WEIGHT ===================================== */
 
-Weight::Weight(Neuron &t_source, Neuron &t_destination)
+Weight::Weight(Neuron& t_source, Neuron& t_destination)
     : source{t_source}, destination{t_destination}
 {
     rng_engine r{};
-    rng_distribution defaultRange{-5.5, 5.5};
+    rng_distribution defaultRange{-1.0, 1.0};
     weightValue = defaultRange(r);
 }
 
-Weight::Weight(Neuron &t_source, Neuron &t_destination,
-               double startRange, double endRange)
+Weight::Weight(Neuron& t_source, Neuron& t_destination, double startRange,
+               double endRange)
     : source{t_source}, destination{t_destination}
 {
     rng_engine r{};
     rng_distribution range{startRange, endRange};
     weightValue = range(r);
 }
+
+Neuron Weight::getSource() { return this->source; }
+Neuron Weight::getDestination() { return this->destination; }
+
+void Weight::setSource(Neuron& t_source) { this->source = t_source; }
+void Weight::setDestination(Neuron& t_destination)
+{
+    this->destination = t_destination;
+}
+
+/* ============================== LAYER ===================================== */
