ca6cccac0b8eda8baf1ef34312487322a845df35
Lam T Duong <lamthachduong@gmail.com>
2021-12-07 20:50:25 -0500

    Renewed Attribute.

--- include/Attribute.hpp
+++ include/Attribute.hpp
@@ -8,19 +8,20 @@ this is not neccessary. Only use for multi-label categorical classification.*/
 class Attribute
 {
 public:
-	/* Empty constructor with empty initial values*/
-	Attribute();
+    /* Empty constructor with empty initial values*/
+    Attribute();
 
-	/* Default constructor that takes in a string value and a string name */
-	Attribute(std::string value, std::string name);
+    /* Default constructor that takes in a string value and a string name */
+    Attribute(std::string value, std::string name);
 
-	std::string getValue();
-	std::string getName();
-	void setName(const std::string t_name);
-	void setValue(const std::string t_value);
+    std::string getValue();
+    std::string getName();
+    void setName(const std::string t_name);
+    void setValue(const std::string t_value);
+
 private:
-	std::string attributeValue;
-	std::string attributeName;
+    std::string attributeValue;
+    std::string attributeName;
 };
 
 #endif
--- src/Attribute.cpp
+++ src/Attribute.cpp
@@ -1,34 +1,27 @@
 #include "../include/Attribute.hpp"
 
-
 Attribute::Attribute()
 {
-	this->attributeName = "";
-	this->attributeValue = "";
+    this->attributeName = "";
+    this->attributeValue = "";
 }
 
 Attribute::Attribute(std::string t_value, std::string t_name)
 {
-	this->attributeName = t_name;
-	this->attributeValue = t_value;
+    this->attributeName = t_name;
+    this->attributeValue = t_value;
 }
 
-std::string Attribute::getValue()
-{
-	return this->attributeValue;
-}
+std::string Attribute::getValue() { return this->attributeValue; }
 
-std::string Attribute::getName()
-{
-	return this->attributeName;
-}
+std::string Attribute::getName() { return this->attributeName; }
 
 void Attribute::setName(const std::string t_name)
 {
-	this->attributeName = t_name;
+    this->attributeName = t_name;
 }
 
 void Attribute::setValue(const std::string t_value)
 {
-	this->attributeValue = t_value;
+    this->attributeValue = t_value;
 }
