7ff17e9261865c92f7f3c6c752873956bfb999bc
Lam T Duong <lamthachduong@gmail.com>
2021-12-07 22:58:23 -0500

    Updated examples format.

--- include/Example.hpp
+++ include/Example.hpp
@@ -1,24 +1,25 @@
 #ifndef EXAMPLE_HPP
 #define EXAMPLE_HPP
 
-#include <vector>
-#include <map>
-#include <iostream>
 #include "Attribute.hpp"
-
+#include <iostream>
+#include <map>
+#include <vector>
 
 /* A class made for individual examples for learning.*/
 class Example
 {
 public:
-	Example(std::map<std::string, Attribute> t_attributes, Attribute t_mainAttribute);
-	std::string getAttributeValueString(std::string t_attributeName);
-	double getAttributeValue(std::string t_attributeName);
-	Attribute getMainAttribute();
+    Example(std::map<std::string, Attribute>& t_attributes,
+            Attribute& t_mainAttribute);
+    std::string getAttributeValueString(std::string t_attributeName);
+    double getAttributeValue(std::string t_attributeName);
+    Attribute getMainAttribute();
+
 private:
-	//Attribute attribute;
-	std::map<std::string, Attribute> m_attributes;
-	Attribute m_mainAttribute;
+    // Attribute attribute;
+    std::map<std::string, Attribute> m_attributes;
+    Attribute m_mainAttribute;
 };
 
-#endif 
+#endif
--- src/Example.cpp
+++ src/Example.cpp
@@ -2,28 +2,21 @@
 
 /*Standard constructor for example class. Must provide attributes and target
 attribute.*/
-Example::Example(std::map<std::string, Attribute> t_attributes, Attribute t_mainAttribute) : 
-    m_attributes{ t_attributes }, m_mainAttribute{ t_mainAttribute } {};
+Example::Example(std::map<std::string, Attribute>& t_attributes,
+                 Attribute& t_mainAttribute)
+    : m_attributes{t_attributes}, m_mainAttribute{t_mainAttribute} {};
 
 /*Get the value of the attribute as a string.*/
 std::string Example::getAttributeValueString(std::string t_attributeName)
 {
-	return m_attributes.at(t_attributeName).getValue;
+    return m_attributes.at(t_attributeName).getValue();
 }
 
 /*Get attribute value as a double. Some conversion is required.*/
 double Example::getAttributeValue(std::string t_attributeName)
 {
-	Attribute targetAttribute = m_attributes.at(t_attributeName);
-	if (&targetAttribute == NULL)
-	{
-		throw ("Cannot convert a null attribute to double.\n");
-	}
-	return std::stod(targetAttribute.getValue());
+    auto targetAttribute = m_attributes.at(t_attributeName);
+    return std::stod(targetAttribute.getValue());
 }
 
-Attribute Example::getMainAttribute()
-{
-	return m_mainAttribute;
-}
-
+Attribute Example::getMainAttribute() { return m_mainAttribute; }
