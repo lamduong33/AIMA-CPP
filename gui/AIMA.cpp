8a2c9a4c6393f7540ec325429ec64018faf30b1f
Lam T Duong <lamthachduong@gmail.com>
2022-02-07 18:38:50 -0500

    feat: header file for Qt

--- gui/AIMA.cpp
+++ gui/AIMA.cpp
@@ -1,11 +1,16 @@
-#include <QApplication>
-#include <QLabel>
+#include "../include/AIMAGUI.hpp"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
-    QLabel hello("Hello world!");
 
-    hello.show();
+    QPushButton button;
+    button.setText("Get Started!");
+    button.setToolTip("This will start the program");
+    QFont font("Iosevka");
+    button.setFont(font);
+
+    button.show();
+
     return app.exec();
 }
---
+++ include/AIMAGUI.hpp
@@ -0,0 +1,11 @@
+#define AIMAGUI_HPP_
+#ifndef AIMAGUI_HPP_
+
+#include <QApplication>
+#include <QPushButton>
+#include <QIcon>
+#include <QGraphicsItem>
+
+#include "ANN.hpp"
+
+#endif
