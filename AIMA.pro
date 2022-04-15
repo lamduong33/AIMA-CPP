e5695c5fa2320b4fd73fee885a84c9ebbb65135a
Lam T Duong <lamthachduong@gmail.com>
2022-04-14 21:00:40 -0400

    fix: GUI update

--- AIMA.pro
+++ AIMA.pro
@@ -6,5 +6,5 @@ QT = core gui
 greaterThan(QT_MAJOR_VERESION, 5): QT += widgets
 
 SOURCES += gui/AIMA.cpp
-
+SOURCES += src/*.cpp
 HEADERS += ./include/*.hpp
--- gui/AIMA.cpp
+++ gui/AIMA.cpp
@@ -1,6 +1,14 @@
 #include "../include/AIMAGUI.hpp"
 
-// Hello
+class VWindow : public QWidget
+{
+    Q_OBJECT;
+public:
+    explicit VWindow(QWidget *parent = 0);
+    signals:
+    public slots:
+};
+
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
@@ -8,8 +16,12 @@ int main(int argc, char **argv)
     QWidget window;
     window.setFixedSize(100, 50);
 
-    auto button = new QPushButton("Get Started", &window);
+    auto button = new QPushButton(&window);
+    button->setText("Get started!");
+    button->setToolTip("REALLY get started");
+    button->setFont(QFont("Iosevka"));
     button->setGeometry(10, 10, 80, 30);
+    button->setIcon(QIcon::fromTheme("face-smile"));
 
     window.show();
 
